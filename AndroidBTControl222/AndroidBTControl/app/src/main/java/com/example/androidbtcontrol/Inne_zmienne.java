8ae5c4eef5c9ade7c16afe6a935a232930093bfa
dev_superkulek <mercobenzo@gmail.com>
2018-01-06 15:30:11 +0100

    add project

---
+++ AndroidBTControl222/AndroidBTControl/app/src/main/java/com/example/androidbtcontrol/Inne_zmienne.java
@@ -0,0 +1,77 @@
+/*
+Android Example to connect to and communicate with Bluetooth
+In this exercise, the target is a Arduino Due + HC-06 (Bluetooth Module)
+
+Ref:
+- Make BlueTooth connection between Android devices
+http://android-er.blogspot.com/2014/12/make-bluetooth-connection-between.html
+- Bluetooth communication between Android devices
+http://android-er.blogspot.com/2014/12/bluetooth-communication-between-android.html
+ */
+package com.example.androidbtcontrol;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Inne_zmienne extends AppCompatActivity {
+
+private EditText Edit1,Edit2,Edit3,Edit4,Edit5,Edit6;
+private static String Edit_1,Edit_2,Edit_3,Edit_4,Edit_5,Edit_6;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inne_zmienne_activity);
+        Button btnSave = (Button)findViewById(R.id.btnsave);
+        Edit1 = (EditText) findViewById(R.id.edittext1);
+        Edit2 = (EditText) findViewById(R.id.edittext2);
+        Edit3 = (EditText) findViewById(R.id.edittext3);
+        Edit4 = (EditText) findViewById(R.id.edittext4);
+        Edit5 = (EditText) findViewById(R.id.edittext5);
+        Edit6 = (EditText) findViewById(R.id.edittext6);
+        Edit1.setText(Edit_1);
+        Edit2.setText(Edit_2);
+        Edit3.setText(Edit_3);
+        Edit4.setText(Edit_4);
+        Edit5.setText(Edit_5);
+        Edit6.setText(Edit_6);
+
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Edit_1 = Edit1.getText().toString();
+                Edit_2 = Edit2.getText().toString();
+                Edit_3 = Edit3.getText().toString();
+                Edit_4 = Edit4.getText().toString();
+                Edit_5 = Edit5.getText().toString();
+                Edit_6 = Edit6.getText().toString();
+
+            }
+        });
+
+
+    }
+
+    public static String getDana1() {
+        return Edit_1;
+    }
+    public static String getDana2() {
+        return Edit_2;
+    }
+    public static String getDana3() {
+        return Edit_3;
+    }
+    public static String getDana4() {
+        return Edit_4;
+    }
+    public static String getDana5() {
+        return Edit_5;
+    }
+    public static String getDana6() {
+        return Edit_6;
+    }
+
+}
---
+++ AndroidBTControl222/AndroidBTControl/app/src/main/java/com/example/androidbtcontrol/MainActivity.java
@@ -0,0 +1,438 @@
+/*
+Android Example to connect to and communicate with Bluetooth
+In this exercise, the target is a Arduino Due + HC-06 (Bluetooth Module)
+
+Ref:
+- Make BlueTooth connection between Android devices
+http://android-er.blogspot.com/2014/12/make-bluetooth-connection-between.html
+- Bluetooth communication between Android devices
+http://android-er.blogspot.com/2014/12/bluetooth-communication-between-android.html
+ */
+
+package com.example.androidbtcontrol;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Set;
+import java.util.UUID;
+
+
+
+
+public class
+MainActivity extends ActionBarActivity {
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final Random RANDOM = new Random();
+    private LineGraphSeries<DataPoint> series;
+    private int lastX = 0;
+    BluetoothAdapter bluetoothAdapter;
+
+    ArrayList<BluetoothDevice> pairedDeviceArrayList;
+
+    TextView textInfo, textStatus,Ramka;
+    ListView listViewPairedDevice;
+    LinearLayout inputPane;
+    EditText inputField;
+    Button btnSend, NewActivity,REF;
+    ArrayAdapter<BluetoothDevice> pairedDeviceAdapter;
+    private UUID myUUID;
+    private final String UUID_STRING_WELL_KNOWN_SPP =
+            "00001101-0000-1000-8000-00805F9B34FB";
+
+    ThreadConnectBTdevice myThreadConnectBTdevice;
+    ThreadConnected myThreadConnected;
+    @Override
+    public void onResume() {
+        super.onResume();  // Always call the superclass method first
+        String ramka_ = Inne_zmienne.getDana1() + " " + Inne_zmienne.getDana2() + " " + Inne_zmienne.getDana3() + " " + Inne_zmienne.getDana4() + " " + Inne_zmienne.getDana5() + " " + Inne_zmienne.getDana6();
+        Ramka.setText(ramka_);
+//        new Thread(new Runnable() {
+//
+//            @Override
+//            public void run() {
+//                // we add 100 new entries
+//                for (int i = 0; i < 100; i++) {
+//                    runOnUiThread(new Runnable() {
+//
+//                        @Override
+//                        public void run() {
+//                            addEntry();
+//                        }
+//                    });
+//
+//                    // sleep to slow down the add of entries
+//                    try {
+//                        Thread.sleep(600);
+//                    } catch (InterruptedException e) {
+//                        // manage error ...
+//                    }
+//                }
+//            }
+//        }).start();
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        //data
+        series = new LineGraphSeries<DataPoint>();
+        graph.addSeries(series);
+        //
+        Viewport viewport = graph.getViewport();
+        viewport.setYAxisBoundsManual(true);
+        viewport.setMinY(0);
+        viewport.setMaxY(200);
+        viewport.setScrollable(true);
+
+        textInfo = (TextView) findViewById(R.id.info);
+        textStatus = (TextView) findViewById(R.id.status);
+        listViewPairedDevice = (ListView) findViewById(R.id.pairedlist);
+        NewActivity = (Button) findViewById(R.id.newactivity);
+        inputPane = (LinearLayout) findViewById(R.id.inputpane);
+        inputField = (EditText) findViewById(R.id.input);
+        btnSend = (Button) findViewById(R.id.send);
+        Ramka = (TextView) findViewById(R.id.ramka);
+
+        NewActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Inne_zmienne.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (myThreadConnected != null) {
+
+                    byte[] bytesToSend = inputField.getText().toString().getBytes();
+                    myThreadConnected.write(bytesToSend);
+                }
+            }
+        });
+
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)) {
+            Toast.makeText(this,
+                    "FEATURE_BLUETOOTH NOT support",
+                    Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        //using the well-known SPP UUID
+        myUUID = UUID.fromString(UUID_STRING_WELL_KNOWN_SPP);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this,
+                    "Bluetooth is not supported on this hardware platform",
+                    Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        String stInfo = bluetoothAdapter.getName() + "\n" +
+                bluetoothAdapter.getAddress();
+        textInfo.setText(stInfo);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //Turn ON BlueTooth if it is OFF
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        }
+
+        setup();
+    }
+
+    private void setup() {
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            pairedDeviceArrayList = new ArrayList<BluetoothDevice>();
+
+            for (BluetoothDevice device : pairedDevices) {
+                pairedDeviceArrayList.add(device);
+            }
+
+            pairedDeviceAdapter = new ArrayAdapter<BluetoothDevice>(this,
+                    android.R.layout.simple_list_item_1, pairedDeviceArrayList);
+            listViewPairedDevice.setAdapter(pairedDeviceAdapter);
+
+            listViewPairedDevice.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+                    BluetoothDevice device =
+                            (BluetoothDevice) parent.getItemAtPosition(position);
+                    Toast.makeText(MainActivity.this,
+                            "Name: " + device.getName() + "\n"
+                                    + "Address: " + device.getAddress() + "\n"
+                                    + "BondState: " + device.getBondState() + "\n"
+                                    + "BluetoothClass: " + device.getBluetoothClass() + "\n"
+                                    + "Class: " + device.getClass(),
+                            Toast.LENGTH_LONG).show();
+
+                    textStatus.setText("start ThreadConnectBTdevice");
+                    myThreadConnectBTdevice = new ThreadConnectBTdevice(device);
+                    myThreadConnectBTdevice.start();
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (myThreadConnectBTdevice != null) {
+            myThreadConnectBTdevice.cancel();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == Activity.RESULT_OK) {
+                setup();
+            } else {
+                Toast.makeText(this,
+                        "BlueTooth NOT enabled",
+                        Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    //Called in ThreadConnectBTdevice once connect successed
+    //to start ThreadConnected
+    private void startThreadConnected(BluetoothSocket socket) {
+
+        myThreadConnected = new ThreadConnected(socket);
+        myThreadConnected.start();
+    }
+
+    /*
+    ThreadConnectBTdevice:
+    Background Thread to handle BlueTooth connecting
+    */
+    private class ThreadConnectBTdevice extends Thread {
+
+        private BluetoothSocket bluetoothSocket = null;
+        private final BluetoothDevice bluetoothDevice;
+
+
+        private ThreadConnectBTdevice(BluetoothDevice device) {
+            bluetoothDevice = device;
+
+            try {
+                bluetoothSocket = device.createRfcommSocketToServiceRecord(myUUID);
+                textStatus.setText("bluetoothSocket: \n" + bluetoothSocket);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            boolean success = false;
+            try {
+                bluetoothSocket.connect();
+                success = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+
+                final String eMessage = e.getMessage();
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        textStatus.setText("something wrong bluetoothSocket.connect(): \n" + eMessage);
+                    }
+                });
+
+                try {
+                    bluetoothSocket.close();
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+            }
+
+            if (success) {
+                //connect successful
+                final String msgconnected = "connect successful:\n"
+                        + "BluetoothSocket: " + bluetoothSocket + "\n"
+                        + "BluetoothDevice: " + bluetoothDevice;
+
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        textStatus.setText(msgconnected);
+
+                        listViewPairedDevice.setVisibility(View.GONE);
+                        inputPane.setVisibility(View.VISIBLE);
+                    }
+                });
+
+                startThreadConnected(bluetoothSocket);
+            } else {
+                //fail
+            }
+        }
+
+        public void cancel() {
+
+            Toast.makeText(getApplicationContext(),
+                    "close bluetoothSocket",
+                    Toast.LENGTH_LONG).show();
+
+            try {
+                bluetoothSocket.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    /*
+    ThreadConnected:
+    Background Thread to handle Bluetooth data communication
+    after connected
+     */
+    private class ThreadConnected extends Thread {
+        private final BluetoothSocket connectedBluetoothSocket;
+        private final InputStream connectedInputStream;
+        private final OutputStream connectedOutputStream;
+
+        public ThreadConnected(BluetoothSocket socket) {
+            connectedBluetoothSocket = socket;
+            InputStream in = null;
+            OutputStream out = null;
+
+            try {
+                in = socket.getInputStream();
+                out = socket.getOutputStream();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            connectedInputStream = in;
+            connectedOutputStream = out;
+        }
+
+        // int twojamama = 0;
+        String checkSumString;
+        @Override
+        public void run() {
+            byte[] buffer = new byte[1024];
+            int bytes;
+
+            while (true) {
+                try {
+                    bytes = connectedInputStream.read(buffer);
+                    String strReceived = new String(buffer, 0, bytes);
+                    boolean isChecksum = strReceived.contains("SK");
+
+                    if(isChecksum){
+                        String[] checkSumArray = strReceived.split("K");
+                        checkSumString = checkSumArray[1];
+                        runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                Ramka.setText("suma kontrolna: " + checkSumString);
+                                double currentBalanceDbl = Double.parseDouble(checkSumString);
+                                series.appendData(new DataPoint(lastX++, currentBalanceDbl), true, 10);
+                            }
+                        });
+                    }
+                    //twojamama++;
+
+
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+
+                    final String msgConnectionLost = "Connection lost:\n"
+                            + e.getMessage();
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            textStatus.setText(msgConnectionLost);
+                        }
+                    });
+                }
+            }
+        }
+
+        public void write(byte[] buffer) {
+            try {
+                connectedOutputStream.write(buffer);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        public void cancel() {
+            try {
+                connectedBluetoothSocket.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+    private void addEntry() {
+        // here, we choose to display max 10 points on the viewport and we scroll to end
+        series.appendData(new DataPoint(lastX++, RANDOM.nextDouble() * 10d), true, 10);
+    }
+}
